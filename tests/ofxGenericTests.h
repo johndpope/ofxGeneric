a7635211a517ae4c1567001f3596683b0599e8c9
Ian G <yo.ian.g@gmail.com>
2012-02-28 11:17:16 -0800

    initial commit with unit tests, not linking :(

---
+++ tests/ofxGenericTests.h
@@ -0,0 +1,13 @@
+//
+//  ofxGenericTests.h
+//  ofxGenericTests
+//
+//  Created by Ian Grossberg on 2/27/12.
+//  Copyright (c) 2012 Lumos Labs. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface ofxGenericTests : SenTestCase
+
+@end
---
+++ tests/ofxGenericTests.mm
@@ -0,0 +1,32 @@
+//
+//  ofxGenericTests.m
+//  ofxGenericTests
+//
+//  Created by Ian Grossberg on 2/27/12.
+//  Copyright (c) 2012 Lumos Labs. All rights reserved.
+//
+
+#import "ofxGenericTests.h"
+
+@implementation ofxGenericTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in ofxGenericTests");
+}
+
+@end
