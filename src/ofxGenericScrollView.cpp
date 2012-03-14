cf0497b0b4864b58a67b47725b672e3104c841d8
Ian G <yo.ian.g@gmail.com>
2012-03-14 16:31:41 -0700

    ofxGenericScrollView

--- src/ofxGenericScrollView.cpp
+++ src/ofxGenericScrollView.cpp
@@ -6,4 +6,57 @@
 //  Copyright (c) 2012 Lumos Labs. All rights reserved.
 //
 
-#include <iostream>
+#include "ofxGenericScrollView.h"
+#include "ofxGenericUtility.h"
+
+ofxGenericScrollView::ofxGenericScrollView()
+{
+}
+
+ofPtr< ofxGenericScrollView > ofxGenericScrollView::create( const ofRectangle& setFrame )
+{
+    ofPtr< ofxGenericScrollView > create = ofPtr< ofxGenericScrollView >( new ofxGenericScrollView() );
+    create->init( create, setFrame );
+    return create;
+}
+
+NativeView ofxGenericScrollView::createNativeView( const ofRectangle& frame )
+{
+#if TARGET_OS_IPHONE
+    UIScrollView* newView = [ [ UIScrollView alloc ] initWithFrame:ofRectangleToCGRect( frame ) ];
+    [ newView setBackgroundColor:[ UIColor clearColor ] ];
+    return newView;
+#elif TARGET_ANDROID
+    return ofxGenericView::createNativeView( frame );
+#endif
+}
+
+void ofxGenericScrollView::setContentSize( const ofPoint& contentSize )
+{
+#if TARGET_OS_IPHONE
+    UIScrollView* view = ( UIScrollView* )*this;
+    if ( view )
+    {
+        [ view setContentSize:CGSizeMake( contentSize.x, contentSize.y ) ];
+    }
+#elif TARGET_ANDROID
+#endif
+}
+
+void ofxGenericScrollView::setContentOffset( const ofPoint& contentOffset, bool animated )
+{
+#if TARGET_OS_IPHONE
+    UIScrollView* view = ( UIScrollView* )*this;
+    if ( view )
+    {
+        [ view setContentOffset:CGPointMake( contentOffset.x, contentOffset.y ) animated:( BOOL )animated ];
+    }
+#elif TARGET_ANDROID
+#endif    
+}
+
+#if TARGET_OS_IPHONE
+
+ofxGenericUIViewCastOperator( ofxGenericScrollView, UIScrollView );
+
+#endif
--- src/ofxGenericScrollView.h
+++ src/ofxGenericScrollView.h
@@ -6,9 +6,23 @@
 //  Copyright (c) 2012 Lumos Labs. All rights reserved.
 //
 
-#ifndef ofxGeneric_ofxGenericScrollView_h
-#define ofxGeneric_ofxGenericScrollView_h
+#pragma once
 
+#include "ofxGenericView.h"
 
-
+class ofxGenericScrollView : public ofxGenericView
+{
+public:
+    static ofPtr< ofxGenericScrollView > create( const ofRectangle& setFrame = ofRectangle( 0, 0, 0, 0 ) );
+    
+    void setContentSize( const ofPoint& contentSize );
+    void setContentOffset( const ofPoint& contentOffset, bool animated );
+    
+#if TARGET_OS_IPHONE
+    operator UIScrollView*();
 #endif
+    
+protected:
+    ofxGenericScrollView();
+    virtual NativeView createNativeView( const ofRectangle& frame );
+};
