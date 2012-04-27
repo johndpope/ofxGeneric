7ea2ce6fc027bab9018f74b551da4ade3edaf3c3
Eli <eli@lumoslabs.com>
2012-04-27 15:55:05 -0700

    ofxGenericDrawableView added.

---
+++ src/ofxGenericDrawableView.cpp
@@ -0,0 +1,235 @@
+//
+//  ofxDrawing.cpp
+//  ofxGeneric
+//
+//  Created by Eli Delventhal on 4/26/12.
+//  Copyright (c) 2012 Lumos Labs. All rights reserved.
+//
+
+#include "ofxGenericDrawableView.h"
+#include "ofUtils.h"
+
+ofxGenericDrawableView::~ofxGenericDrawableView()
+{
+    
+}
+
+ofxGenericDrawableView::ofxGenericDrawableView()
+{
+    
+}
+
+ofPtr < ofxGenericDrawableView > ofxGenericDrawableView::create( const ofRectangle &rect)
+{
+    ofPtr< ofxGenericDrawableView > c = ofPtr< ofxGenericDrawableView > ( new ofxGenericDrawableView() );
+    c->init( c, rect );
+    return c;
+}
+
+void ofxGenericDrawableView::setLineWidth(float w)
+{
+    _drawCalls.push_back( ofxGenericDrawCall( w ) );
+/*#if TARGET_OS_IPHONE
+    CGContextSetLineWidth(UIGraphicsGetCurrentContext(), w);
+#endif*/
+    
+}
+
+void ofxGenericDrawableView::setStrokeColor( const ofColor &c )
+{
+    _drawCalls.push_back( ofxGenericDrawCall( c ) );
+/*#if TARGET_OS_IPHONE
+    CGContextSetStrokeColorWithColor(UIGraphicsGetCurrentContext(), [UIColor colorWithRed:c.r green:c.g blue:c.b alpha:c.a].CGColor);
+#endif    */
+}
+
+void ofxGenericDrawableView::drawLine( const ofPoint &p1, const ofPoint &p2 )
+{
+    _drawCalls.push_back( ofxGenericDrawCall( p1, p2 ) );
+/*#if TARGET_OS_IPHONE
+    CGContextRef context = UIGraphicsGetCurrentContext();
+    CGContextBeginPath(context);
+    CGContextMoveToPoint(context, p1.x, p1.y);
+    CGContextAddLineToPoint(context, p2.x, p2.y);
+    CGContextStrokePath(context);
+#endif    */
+}
+
+void ofxGenericDrawableView::drawLines( std::vector< ofPoint > points )
+{
+    for (int i = 0; i < points.size(); i++)
+    {
+        _drawCalls.push_back( ofxGenericDrawCall( points[0].x, points[1].y ) );
+    }
+    
+/*#if TARGET_OS_IPHONE
+    if (points.size() > 0)
+    {
+        CGContextRef context = UIGraphicsGetCurrentContext();
+        CGContextBeginPath(context);
+        CGContextMoveToPoint(context, points[0].x, points[1].y);
+        
+        for (int i = 0; i < (int) points.size(); i++)
+        {
+            CGContextAddLineToPoint(context, points[i].x, points[i].y);
+        }
+        
+        CGContextStrokePath(context);
+    }
+#endif  */
+    
+}
+
+#if TARGET_OS_IPHONE
+UIView* ofxGenericDrawableView::allocNativeView( const ofRectangle& setFrame )
+{
+    return [ [ ofxDrawableUIView alloc ] init:CGRectMake(setFrame.x, setFrame.y, setFrame.width, setFrame.height) owner: dynamic_pointer_cast< ofxGenericDrawableView >(_this) ];
+}
+#elif TARGET_ANDROID
+jobject ofxGenericDrawableView::allocNativeView( const ofRectangle& setFrame )
+{
+    return ofxGenericView::allocNativeView( setFrame );
+}
+#endif
+
+void ofxGenericDrawableView::repaint()
+{
+#if TARGET_OS_IPHONE
+    [(ofxDrawableUIView *)_view setNeedsDisplay];
+#endif
+    _drawCalls.clear();
+}
+
+int ofxGenericDrawableView::getDrawCallCount()
+{
+    return _drawCalls.size();
+}
+
+ofxGenericDrawCall ofxGenericDrawableView::getDrawCallAt(int i)
+{
+    return _drawCalls[i];
+}
+
+//////////////////////////ofxGenericDrawCall////////////////////////
+
+ofxGenericDrawCall::~ofxGenericDrawCall()
+{
+    
+}
+
+ofxGenericDrawCall::ofxGenericDrawCall( ofPoint startPoint, ofPoint endPoint )
+{
+    start = startPoint;
+    end = endPoint;
+    newPosition = true;
+    newColor = false;
+    newLineWidth = false;
+}
+
+ofxGenericDrawCall::ofxGenericDrawCall( float wid )
+{
+    lineWidth = wid;
+    newPosition = false;
+    newColor = false;
+    newLineWidth = true;
+}
+
+ofxGenericDrawCall::ofxGenericDrawCall( ofColor col )
+{
+    color = col;
+    newPosition = false;
+    newColor = true;
+    newLineWidth = false;
+}
+
+bool ofxGenericDrawCall::changesLineWidth()
+{
+    return newLineWidth;
+}
+
+bool ofxGenericDrawCall::changesColor()
+{
+    return newColor;
+}
+
+bool ofxGenericDrawCall::changesPosition()
+{
+    return newPosition;
+}
+
+ofPoint ofxGenericDrawCall::getStart()
+{
+    return start;
+}
+
+ofPoint ofxGenericDrawCall::getEnd()
+{
+    return end;
+}
+
+float ofxGenericDrawCall::getLineWidth()
+{
+    return lineWidth;
+}
+
+ofColor ofxGenericDrawCall::getColor()
+{
+    return color;
+}
+
+////////////////////////////ofxDrawableUIView////////////////////////////
+
+#if TARGET_OS_IPHONE
+@implementation ofxDrawableUIView
+
+- (id) init:(CGRect)frame owner:(ofPtrWeak< ofxGenericDrawableView >)own
+{
+    if (self = [super initWithFrame:frame])
+    {
+        drawOwner = own;
+        return self;
+    }
+    return nil;
+}
+
+- (void) drawRect:(CGRect)rect
+{
+    ofPtr< ofxGenericDrawableView > own = drawOwner.lock();
+    if (own->getDrawCallCount() > 0)
+    {
+        CGContextRef context = UIGraphicsGetCurrentContext();
+        CGContextBeginPath(context);
+        
+        ofPoint lastPoint = ofPoint(-1,-1);
+        
+        for (int i = 0; i < own->getDrawCallCount(); i++)
+        {
+            ofxGenericDrawCall call = own->getDrawCallAt(i);
+            
+            if (call.changesLineWidth())
+            {
+                CGContextSetLineWidth(context, call.getLineWidth());
+            }
+            
+            if (call.changesColor())
+            {
+                ofColor c = call.getColor();
+                CGContextSetStrokeColorWithColor(context, [UIColor colorWithRed:c.r green:c.g blue:c.b alpha:c.a].CGColor);
+            }
+            
+            if (call.changesPosition())
+            {
+                if (call.getStart() != lastPoint)
+                {
+                    CGContextMoveToPoint(context, call.getStart().x, call.getStart().y);
+                }
+                
+                CGContextAddLineToPoint(context, call.getEnd().x, call.getEnd().y);
+                lastPoint = call.getEnd();
+            }
+        }
+    }
+}
+
+@end
+#endif
---
+++ src/ofxGenericDrawableView.h
@@ -0,0 +1,83 @@
+//
+//  ofxDrawing.h
+//  ofxGeneric
+//
+//  Created by Eli Delventhal on 4/26/12.
+//  Copyright (c) 2012 Lumos Labs. All rights reserved.
+//
+
+#pragma once
+
+#include "ofxGenericView.h"
+
+#if TARGET_OS_IPHONE
+#include <UIKit/UIKit.h>
+#endif
+
+class ofxGenericDrawCall;
+
+class ofxGenericDrawableView : public ofxGenericView
+{
+public:
+    virtual ~ofxGenericDrawableView();
+    static ofPtr < ofxGenericDrawableView > create( const ofRectangle &rect = ofRectangle(0, 0, 0, 0) );
+    
+    virtual void setLineWidth(float w);
+    virtual void setStrokeColor( const ofColor &c );
+    virtual void drawLine( const ofPoint &p1, const ofPoint &p2 );
+    virtual void drawLines( std::vector< ofPoint > points );
+    
+    //clears all pending draw calls and updates the drawing context
+    virtual void repaint();
+    
+    virtual int getDrawCallCount();
+    virtual ofxGenericDrawCall getDrawCallAt(int i);
+    
+protected:
+    ofxGenericDrawableView();
+
+    std::vector< ofxGenericDrawCall > _drawCalls;
+    
+#if TARGET_OS_IPHONE
+    virtual UIView* allocNativeView( const ofRectangle& frame );
+#elif TARGET_ANDROID
+    virtual jobject allocNativeView( const ofRectangle& frame );
+#endif
+};
+
+class ofxGenericDrawCall
+{
+public:
+    ofxGenericDrawCall( ofPoint start, ofPoint end );
+    ofxGenericDrawCall( float lineWidth );
+    ofxGenericDrawCall( ofColor color );
+    ~ofxGenericDrawCall();
+    bool changesLineWidth();
+    bool changesColor();
+    bool changesPosition();
+    ofPoint getStart();
+    ofPoint getEnd();
+    float getLineWidth();
+    ofColor getColor();
+    
+protected:
+    ofPoint start;
+    ofPoint end;
+    float lineWidth;
+    ofColor color;
+    bool newLineWidth;
+    bool newColor;
+    bool newPosition;
+};
+
+#if TARGET_OS_IPHONE
+@interface ofxDrawableUIView : UIView
+{
+    ofPtrWeak< ofxGenericDrawableView > drawOwner;
+}
+
+- (id) init:(CGRect)frame owner:(ofPtrWeak< ofxGenericDrawableView >)own;
+
+@end
+
+#endif
