1cd94f4b6f8e12911371c8328ee8beaff65d8caf
Ian G <yo.ian.g@gmail.com>
2012-05-07 11:59:59 -0700

    ofxGenericSettings

---
+++ src/ofxGenericSettings.cpp
@@ -0,0 +1,35 @@
+//
+//  ofxGenericSettings.cpp
+//  ofxGeneric
+//
+//  Created by Ian Grossberg on 5/7/12.
+//  Copyright (c) 2012 Lumos Labs. All rights reserved.
+//
+
+#include "ofxGenericSettings.h"
+#include "ofxGenericCache.h"
+
+ofxGenericSettings::~ofxGenericSettings()
+{
+}
+
+void ofxGenericSettings::init( ofPtrWeak< ofxGenericSettings > setThis )
+{
+    _this = setThis;
+}
+
+void ofxGenericSettings::load()
+{
+    ofPtr< ofxGenericCache > loaded = ofxGenericCache::create( false );
+    loaded->setFileName( getFileName(), false );
+    
+    // TODO: merge binary's settings with downloaded settings
+    if ( loaded->readFromDisk() )
+    {
+        parse( loaded );
+    }
+}
+
+ofxGenericSettings::ofxGenericSettings()
+{    
+}
---
+++ src/ofxGenericSettings.h
@@ -0,0 +1,29 @@
+//
+//  ofxGenericSettings.h
+//  ofxGeneric
+//
+//  Created by Ian Grossberg on 5/7/12.
+//  Copyright (c) 2012 Lumos Labs. All rights reserved.
+//
+
+#pragma once
+
+#include "ofxGenericMain.h"
+
+class ofxGenericCache;
+
+class ofxGenericSettings
+{
+public:
+    virtual ~ofxGenericSettings();
+    
+    void load();
+    
+protected:
+    ofxGenericSettings();
+    virtual void init( ofPtrWeak< ofxGenericSettings > setThis );
+    ofPtrWeak< ofxGenericSettings > _this;
+
+    virtual string getFileName() = 0;
+    virtual void parse( ofPtr< ofxGenericCache >& settings ) = 0;
+};
