6b20c6b8d40561ab1a5257b2b18dcfe2082eb2d5
Ian G <yo.ian.g@gmail.com>
2012-06-10 21:21:38 -0700

    Cache Utility for common cache interps

---
+++ src/ofxGenericCacheUtility.h
@@ -0,0 +1,13 @@
+//
+//  ofxGenericCacheUtility.h
+//  ofxGeneric
+//
+//  Created by Ian Grossberg on 6/8/12.
+//  Copyright (c) 2012 Lumos Labs. All rights reserved.
+//
+
+#pragma once
+
+#include "ofxGenericCache.h"
+
+ofColor toofColor( ofPtr< ofxGenericCache > from );
