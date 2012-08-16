6d0eaed787a31193bccd5503a5c0c4205bee12ab
Ian G <yo.ian.g@gmail.com>
2012-08-15 23:44:58 -0700

    separate table cell from table view file into its own file

--- src/ofxGenericTableView.cpp
+++ src/ofxGenericTableView.cpp
@@ -6,6 +6,9 @@
 //
 
 #include "ofxGenericTableView.h"
+
+#include "ofxGenericTableViewCell.h"
+
 #include "ofxGenericUtility.h"
 
 #if TARGET_OS_IPHONE
@@ -53,22 +56,6 @@ NativeView ofxGenericTableView::createNativeView( const ofRectangle& frame )
     
     return newView;
 #endif
-}
-
-void ofxGenericTableViewCell::setTablePosition( unsigned int section, unsigned int index )
-{
-    _section = section;
-    _index = index;
-}
-
-unsigned int ofxGenericTableViewCell::getSection()
-{
-    return _section;
-}
-
-unsigned int ofxGenericTableViewCell::getIndex()
-{
-    return _index;
 }
 
 unsigned int ofxGenericTableView::getNumberOfCells( unsigned int section )
@@ -450,146 +437,6 @@ void ofxGenericTableView::scrollToCell( unsigned int section, unsigned int index
 
 #if TARGET_OS_IPHONE
 ofxGenericUIViewCastOperator( ofxGenericTableView, UITableView );
-#endif
-
-////////////////////////////////////////////////////////////////////////////////////////////////////
-
-ofxGenericTableViewCell::~ofxGenericTableViewCell()
-{
-#if TARGET_OS_IPHONE
-    releaseView( _view );
-#endif
-}
-
-ofPtr< ofxGenericTableViewCell > ofxGenericTableViewCell::create( ofPtrWeak< ofxGenericTableView > table, unsigned int section, unsigned int index, const ofRectangle& setBounds  )
-{
-    ofPtr< ofxGenericTableViewCell > c = ofPtr< ofxGenericTableViewCell >( new ofxGenericTableViewCell() );
-    c->init(c, table, section, index, setBounds);
-    return c;
-}
-
-ofxGenericTableViewCell::ofxGenericTableViewCell()
-: _section( 0 ), _index( 0 )
-{
-    
-}
-
-void ofxGenericTableViewCell::init( ofPtrWeak< ofxGenericTableViewCell > setThis, ofPtrWeak< ofxGenericTableView > table, unsigned int section, unsigned int index, const ofRectangle& setBounds )
-{
-    _table = table;
-    _section = section;
-    _index = index;
-    ofxGenericView::init( setThis );
-}
-
-ofPtr< ofxGenericView > ofxGenericTableViewCell::getContentView()
-{
-    return _contentView;
-}
-
-void ofxGenericTableViewCell::selected()
-{
-}
-
-bool ofxGenericTableViewCell::canBeDragged()
-{
-    return false;
-}
-
-
-void ofxGenericTableViewCell::addChildView( ofPtr< ofxGenericView > add )
-{
-    if ( _contentView )
-    {
-        _contentView->addChildView( add );
-    }
-    else
-    {
-        ofxGenericView::addChildView( add );
-    }
-}
-
-void ofxGenericTableViewCell::addChildViewBefore( ofPtr< ofxGenericView > add, ofPtr< ofxGenericView > before )
-{
-    if ( _contentView )
-    {
-        _contentView->addChildViewBefore( add, before );
-    }
-    else
-    {
-        ofxGenericView::addChildViewBefore( add, before );
-    }
-}
-
-void ofxGenericTableViewCell::addChildViewAfter( ofPtr< ofxGenericView > add, ofPtr< ofxGenericView > after )
-{
-    if ( _contentView )
-    {
-        _contentView->addChildViewAfter( add, after );
-    }
-    else
-    {
-        ofxGenericView::addChildViewAfter( add, after );
-    }
-}
-
-void ofxGenericTableViewCell::removeChildView( ofPtr< ofxGenericView > remove )
-{
-    if ( _contentView )
-    {
-        _contentView->removeChildView( remove );
-    }
-    else
-    {
-        ofxGenericView::removeChildView( remove );
-    }
-}
-
-void ofxGenericTableViewCell::setBackgroundColor( const ofColor& color )
-{
-    if ( _contentView )
-    {
-        _contentView->setBackgroundColor( color );
-    }
-    ofxGenericView::setBackgroundColor( color );
-}
-
-void ofxGenericTableViewCell::setText( string text )
-{
-#if TARGET_OS_IPHONE
-    UITableViewCell* cell = ( UITableViewCell* )_view;
-    if (cell)
-    {
-        cell.textLabel.text = [NSString stringWithCString:text.c_str() encoding:NSUTF8StringEncoding];
-    }
-#endif
-}
-
-void ofxGenericTableViewCell::setImage( string imagePath )
-{
-#if TARGET_OS_IPHONE
-    UITableViewCell* cell = ( UITableViewCell* )_view;
-    if (cell)
-    {
-        cell.imageView.image = [UIImage imageNamed:[NSString stringWithCString:imagePath.c_str() encoding:NSUTF8StringEncoding]];
-    }
-#endif    
-}
-
-NativeView ofxGenericTableViewCell::createNativeView( const ofRectangle& frame )
-{
-#if TARGET_OS_IPHONE
-    UITableViewCell* nativeView = [ [ UITableViewCell alloc ] initWithFrame:ofRectangleToCGRect( frame ) ];
-    _contentView = ofxGenericView::create( CGRectToofRectangle( nativeView.contentView.frame ), nativeView.contentView );
-    [ nativeView setSelectionStyle:UITableViewCellSelectionStyleNone ];
-    
-    return nativeView;
-#endif
-    //TODO android
-}
-
-#if TARGET_OS_IPHONE
-ofxGenericUIViewCastOperator( ofxGenericTableViewCell, UITableViewCell );
 
 @implementation ofxGenericTableViewForwarder
 
---
+++ src/ofxGenericTableViewCell.cpp
@@ -0,0 +1,165 @@
+//
+//  ofxGenericTableViewCell.cpp
+//  ofxGeneric
+//
+//  Created by Ian Grossberg on 8/15/12.
+//  Copyright (c) 2012 Lumos Labs. All rights reserved.
+//
+
+#include "ofxGenericTableViewCell.h"
+
+ofxGenericTableViewCell::~ofxGenericTableViewCell()
+{
+#if TARGET_OS_IPHONE
+    releaseView( _view );
+#endif
+}
+
+ofPtr< ofxGenericTableViewCell > ofxGenericTableViewCell::create( ofPtrWeak< ofxGenericTableView > table, unsigned int section, unsigned int index, const ofRectangle& setBounds  )
+{
+    ofPtr< ofxGenericTableViewCell > c = ofPtr< ofxGenericTableViewCell >( new ofxGenericTableViewCell() );
+    c->init(c, table, section, index, setBounds);
+    return c;
+}
+
+ofxGenericTableViewCell::ofxGenericTableViewCell()
+: _section( 0 ), _index( 0 )
+{
+    
+}
+
+void ofxGenericTableViewCell::init( ofPtrWeak< ofxGenericTableViewCell > setThis, ofPtrWeak< ofxGenericTableView > table, unsigned int section, unsigned int index, const ofRectangle& setBounds )
+{
+    _table = table;
+    _section = section;
+    _index = index;
+    ofxGenericView::init( setThis );
+}
+
+
+
+void ofxGenericTableViewCell::setTablePosition( unsigned int section, unsigned int index )
+{
+    _section = section;
+    _index = index;
+}
+
+unsigned int ofxGenericTableViewCell::getSection()
+{
+    return _section;
+}
+
+unsigned int ofxGenericTableViewCell::getIndex()
+{
+    return _index;
+}
+
+ofPtr< ofxGenericView > ofxGenericTableViewCell::getContentView()
+{
+    return _contentView;
+}
+
+void ofxGenericTableViewCell::selected()
+{
+}
+
+bool ofxGenericTableViewCell::canBeDragged()
+{
+    return false;
+}
+
+
+void ofxGenericTableViewCell::addChildView( ofPtr< ofxGenericView > add )
+{
+    if ( _contentView )
+    {
+        _contentView->addChildView( add );
+    }
+    else
+    {
+        ofxGenericView::addChildView( add );
+    }
+}
+
+void ofxGenericTableViewCell::addChildViewBefore( ofPtr< ofxGenericView > add, ofPtr< ofxGenericView > before )
+{
+    if ( _contentView )
+    {
+        _contentView->addChildViewBefore( add, before );
+    }
+    else
+    {
+        ofxGenericView::addChildViewBefore( add, before );
+    }
+}
+
+void ofxGenericTableViewCell::addChildViewAfter( ofPtr< ofxGenericView > add, ofPtr< ofxGenericView > after )
+{
+    if ( _contentView )
+    {
+        _contentView->addChildViewAfter( add, after );
+    }
+    else
+    {
+        ofxGenericView::addChildViewAfter( add, after );
+    }
+}
+
+void ofxGenericTableViewCell::removeChildView( ofPtr< ofxGenericView > remove )
+{
+    if ( _contentView )
+    {
+        _contentView->removeChildView( remove );
+    }
+    else
+    {
+        ofxGenericView::removeChildView( remove );
+    }
+}
+
+void ofxGenericTableViewCell::setBackgroundColor( const ofColor& color )
+{
+    if ( _contentView )
+    {
+        _contentView->setBackgroundColor( color );
+    }
+    ofxGenericView::setBackgroundColor( color );
+}
+
+void ofxGenericTableViewCell::setText( string text )
+{
+#if TARGET_OS_IPHONE
+    UITableViewCell* cell = ( UITableViewCell* )_view;
+    if (cell)
+    {
+        cell.textLabel.text = [NSString stringWithCString:text.c_str() encoding:NSUTF8StringEncoding];
+    }
+#endif
+}
+
+void ofxGenericTableViewCell::setImage( string imagePath )
+{
+#if TARGET_OS_IPHONE
+    UITableViewCell* cell = ( UITableViewCell* )_view;
+    if (cell)
+    {
+        cell.imageView.image = [UIImage imageNamed:[NSString stringWithCString:imagePath.c_str() encoding:NSUTF8StringEncoding]];
+    }
+#endif
+}
+
+NativeView ofxGenericTableViewCell::createNativeView( const ofRectangle& frame )
+{
+#if TARGET_OS_IPHONE
+    UITableViewCell* nativeView = [ [ UITableViewCell alloc ] initWithFrame:ofRectangleToCGRect( frame ) ];
+    _contentView = ofxGenericView::create( CGRectToofRectangle( nativeView.contentView.frame ), nativeView.contentView );
+    [ nativeView setSelectionStyle:UITableViewCellSelectionStyleNone ];
+    
+    return nativeView;
+#endif
+    //TODO android
+}
+
+#if TARGET_OS_IPHONE
+ofxGenericUIViewCastOperator( ofxGenericTableViewCell, UITableViewCell );
+#endif
