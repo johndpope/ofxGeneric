366812e0709c1a5021f8c5968acbbe702d1add7c
Ian G <yo.ian.g@gmail.com>
2012-09-24 13:28:03 -0700

    get centered square fit

--- src/ofxGenericUtility.h
+++ src/ofxGenericUtility.h
@@ -71,6 +71,8 @@ int ofxGRandom();
 int ofxGRandomInRange( int minimum, int maximum );
 int ofxGRandomInRangeExclusive( int minimumInclusive, int maximumExclusive );
 
+ofRectangle ofxGGetCenteredSquareFit( const ofRectangle& rectangle );
+
 //////////////////////////////// HTTP //////////////////////////////////
 
 string ofxGMIMETypeToString( ofxGenericMIMEType type );
