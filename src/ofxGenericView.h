65e26e7e058004b648d47a73cb208e41b0bfa068
Patrick Barry <patrick@lumoslabs.com>
2013-07-26 17:24:48 -0700

    Revert "ofxGenericView - ability for forward UIView::drawRect calls to C++"
    This reverts commit 941f21523a19aee5f2f542da1e9ab660b77e07d2.

--- src/ofxGenericView.h
+++ src/ofxGenericView.h
@@ -159,11 +159,8 @@ public:
     
     static void replaceViewWithView( ofPtr< ofxGenericView > replace, ofPtr< ofxGenericView > with );
     virtual void copyProperties( ofPtr< ofxGenericView > view );
-
-    virtual void forceRedraw();
-    virtual void onRedraw( ofRectangle area );
-
-protected:
+    
+protected:        
     ofxGenericView();
     virtual void init( ofPtrWeak< ofxGenericView > setThis, const ofRectangle& setBounds = ofRectangle( 0, 0, 0, 0 ), NativeView nativeView = NativeNull );
     
@@ -212,8 +209,6 @@ protected:
     ofPtrWeak< ofxGenericView > _parent;
     ofPtrWeak < ofxGenericViewDelegate > _viewDelegate;
 
-    virtual void setForwardOnRedraw( bool enabled );
-    
     friend class ofxAppGenericWindow;
     
 private:
